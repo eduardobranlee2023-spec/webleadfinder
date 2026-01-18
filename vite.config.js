76ca470356d3ad35c3d5c47cdd60073456f5b45b
eduardobranlee2023-spec <eduardobranlee2023@gmail.com>
2026-01-18 13:25:21 -0300

    Agregando archivo de configuracion vite

--- vite.config.js
+++ vite.config.js
@@ -1,7 +1,14 @@
 import { defineConfig } from 'vite'
 import react from '@vitejs/plugin-react'
 
-// https://vite.dev/config/
+// Este archivo es el "traductor" que Vercel necesita para entender tu código de React
 export default defineConfig({
   plugins: [react()],
+  server: {
+    port: 3000
+  },
+  build: {
+    // Esto asegura que los archivos finales se guarden en una carpeta que Vercel entiende
+    outDir: 'dist',
+  }
 })
